fb35f90aef01c59a9ca6b2010ec4ebc3794fc154
Jay McCarthy <jay.mccarthy@gmail.com>
2014-07-07 11:49:09 -0400

    update deps

--- info.rkt
+++ info.rkt
@@ -1,4 +1,4 @@
 #lang info
 (define collection 'multi)
-(define deps '("at-exp-lib" "base" "scribble-text-lib"))
+(define deps '("at-exp-lib" "base" "scribble-text-lib" "scheme-lib"))
 (define build-deps '("racket-doc" "scribble-lib"))
